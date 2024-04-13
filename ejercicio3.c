75f29af0bdc3569a9a839e7ec1cedcdff1c6ae5b
mvsa19a <mvivanegas@uamv.edu.ni>
2024-04-12 21:25:06 -0600

    ejercicio 3

---
+++ ejercicio3.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char nombre[60];
+    printf ("Dime tu nombre: ");
+    scanf ("%s", nombre);
+
+    Printf ("Hola, %s", nombre);
+    return 0;
+}
