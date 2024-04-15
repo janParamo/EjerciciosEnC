8bc943768ba3aa623ade82ff2d1c97a96fac2c08
mvsa19a <mvivanegas@uamv.edu.ni>
2024-04-14 22:49:27 -0600

    ejericio4

---
+++ ejercicio4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float num1, num2, result1, result2;
+
+    printf("Dame el primer numero: ");
+    scanf("%f", &num1);
+
+    printf("Dame el segundo numero: ");
+    scanf("%f", &num2);
+
+    result1 = pow(num1, 4);
+    result2 = pow(num2, 2);
+
+    printf("El primer numero elevado a la cuarta potencia es: %.2f\n", result1);
+    printf("El segundo numero elevado al cuadrado es: %.2f\n", result2);
+
+    return 0;
+}
